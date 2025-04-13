7ee1c2b537ec9ea762f50713ca966f2531b3fc61
Duduubas <eduardobritogomes70@gmail.com>
2025-04-12 21:59:11 -0300

    ✨ added: favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,14 @@
 import type { Metadata } from "next";
 import './globals.css'
 import NoScript from '@/components/noScript';
+import Icon from "@/public/qr-code.png"
 
 export const metadata: Metadata = {
   title: "Gerador de QR Code | Gratuito",
   description: "Gere QR Codes de forma simples e rápida — em menos de 1 minuto!",
+  icons: {
+    icon: Icon.src,
+  },
 };
 
 export default function RootLayout({
